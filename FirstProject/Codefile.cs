0e411ecf287aa8e53110e3fad2771cd9f51a7b5f
Eren Topaçoğlu <93638566+Reactive21@users.noreply.github.com>
2022-10-20 19:58:31 +0300

    Create Codefile.cs

---
+++ FirstProject/Codefile.cs
@@ -0,0 +1,6 @@
+public class Listers
+{
+    public static List<string> names = new List<string>();
+    public static List<string> surnames = new List<string>();
+    public static List<string> no = new List<string>();
+}
